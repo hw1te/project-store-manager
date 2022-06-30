f53ea9e99d21689ad1cb11f928bf6c95b6337c1f
Gustavo Pospi <gutxander@gmail.com>
2022-06-30 03:58:16 -0300

    Commit inicial, abrindo PR.

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ require('dotenv').config();
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
+  console.log('Primeiro commit :)');
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
